e0500569ac9cad0df40875f0d2821839130b97b2
Salih Bedelce <salih.bedelce@uni-ulm.de>
2021-06-16 14:37:37 +0200

    first commit ng new app with employees and navbar component and a service for the laravel api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import{HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/employees/employees.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/service/data.service';
+
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+  employees:any;
+  constructor(private dataService:DataService) { }
+
+  ngOnInit(): void {
+    this.getEmployeesData();
+  }
+  getEmployeesData(){
+    this.dataService.getData().subscribe(res =>{
+      this.employees=res;
+    });
+  }
+}
